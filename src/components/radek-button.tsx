b34f22a47516d1213215a1a9597f6cf603efe2f5
chamik <jakub.hampl@email.cz>
2022-12-18 22:46:47 +0100

    RADEK BUTTON

--- src/components/radek-button.tsx
+++ src/components/radek-button.tsx
@@ -1,10 +1,13 @@
 import type { Component } from 'solid-js';
 import { radekCount, setRadekCount } from '../game';
+import radek from "./../public/media/placeholder_radek.png"
 
 export const RadekButton: Component = () => {
     return (
-        <div>
-            <button onClick={() => setRadekCount(radekCount() + 1)}>RADEK</button>
+        <div class='w-full'>
+            <button class='w-full' onClick={() => setRadekCount(radekCount() + 1)}>
+                <img src={radek} alt="RADEK" width={300} class='mx-auto'/>
+            </button>
         </div>
     );
 };
