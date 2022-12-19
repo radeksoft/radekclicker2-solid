04f1ae7d76ca80fc862bb5c07625047a232e4da1
chamik <jakub.hampl@email.cz>
2022-12-20 00:33:18 +0100

    Added new workers

--- src/workers.ts
+++ src/workers.ts
@@ -1,30 +1,67 @@
+import negr from './public/worker_icons/Negri_icon.png';
+import pec from './public/worker_icons/Pec_icon.png';
+import mama from './public/worker_icons/Vladimirovo_mama_icon.png';
+import lany from './public/worker_icons/Lany_icon.png';
+import mondi from './public/worker_icons/Mondi_icon.png';
+import radeksoft from './public/worker_icons/Radeksoft_icon.png';
+import hultron from './public/worker_icons/Hultron_icon.png';
+import radek from './public/worker_icons/Metaverse_Radek_icon.png';
+
 export const WORKERS = [
+    {
+        name: 'Negři',
+        text: 'Kdepak asi jsou?',
+        img: negr,
+        rps: 0.1,
+        cost: 50,
+    },
     {
         name: 'Výheň',
         text: 'Vypečený Radek',
-        img: 'Pec_icon.png',
+        img: pec,
         rps: 0.1,
         cost: 50,
     },
     {
         name: 'Vladimírovo máma',
         text: 'Přetáhne tě válečkem',
-        img: 'Vladimirovo_mama_icon.png',
+        img: mama,
         rps: 10,
         cost: 200,
     },
     {
         name: 'Lány',
         text: 'Bůhví co se tu pěstuje',
-        img: 'Lany_icon.png',
+        img: lany,
         rps: 10,
         cost: 200,
     },
     {
         name: 'Mondi',
         text: 'Štětí zkurvený',
-        img: 'Mondi_icon.png',
+        img: mondi,
         rps: 10,
         cost: 200,
+    },
+    {
+        name: 'Radeksoft',
+        text: 'Fortune 500 korporát',
+        img: radeksoft,
+        rps: 0.1,
+        cost: 50,
+    },
+    {
+        name: 'Hultron',
+        text: 'směr na Hultron planetu 🎶',
+        img: hultron,
+        rps: 0.1,
+        cost: 50,
+    },
+    {
+        name: 'Metaverse Radek',
+        text: 'Je to Radek',
+        img: radek,
+        rps: 0.1,
+        cost: 50,
     },
 ]
