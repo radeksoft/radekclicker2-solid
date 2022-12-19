3ebfd8c418bd38900ece88676c4ce14b83895552
chamik <jakub.hampl@email.cz>
2022-12-19 21:26:58 +0100

    Redesigned worker

--- src/components/worker.tsx
+++ src/components/worker.tsx
@@ -1,4 +1,4 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 import { radekCount, setRadekCount, radeksPerSecond, setRadeksPerSecond } from '../game';
 import { WORKERS } from '../workers';
 import radek from "./../public/media/placeholder_radek.png"
@@ -26,20 +26,48 @@ export const Worker: Component<WorkerProps> = props => {
 
     const buyable = () => radekCount() >= cost;
 
+    const [anim, setAnim] = createSignal(false);
+
+    const textOverflow = (n: number) => {
+        if (n < 100)
+            return n.toFixed(1);
+        
+        n = Math.floor(n);
+    
+        if (n < 9_000)
+            return n.toString();
+        else if (n < 999_000)
+            return `${(n / 1_000).toFixed(1)} tisíc`;
+        else if (n < 999_000_000)
+            return `${(n / 1_000_000).toFixed(2)} milionů`;
+        else if (n < 999_000_000_000)
+            return `${(n / 1_000_000_000).toFixed(2)} miliard`;
+        else if (n < 999_000_000_000_000)
+            return `${(n / 1_000_000_000_000).toFixed(2)} bilionů`;
+        else if (n < 999_000_000_000_000_000)
+            return `${(n / 1_000_000_000_000_000).toFixed(2)} kvadrilionů`;
+        else
+            return 'kurva hodně';
+    }
+
     return (
-        <div class='flex flex-row border border-black items-center rounded'>
+        <div class='flex flex-row border-4 border-slate-300 items-center rounded bg-slate-200'>
             <img src={radek} alt="RADEK" class='relative m-3 rounded-lg overflow-hidden w-40' />
             <div class='flex flex-col w-full pr-3'>
                 <div class='pr-3'>
                     <div class='flex flex-col'>
-                        <h3 class='mt-3'>{name}</h3>
-                        <p class='text-gray-800 text-xs font-extrabold'>{rps} R/s</p>
+                        <h3 class='mt-3 font-bold'>{name}</h3>
+                        <p class='text-gray-800 text-xs font-extrabold -mt-1 pb-1'>{rps} R/s</p>
                     </div>
                     <p class='text-sm'>{text}</p>
                 </div>
                 
-                <button class={`border shadow my-3 p-1 rounded mx-auto w-full bg-green-400 opacity-40 ${!buyable() && 'cursor-default'} ${buyable() && 'opacity-100 hover:bg-green-500 '}`} onClick={() => buyMe()}>
-                    <span class='font-extrabold'>{cost} R</span>
+                <button class={`border shadow my-3 p-1 rounded mx-auto w-full bg-blue-300 opacity-40 ${!buyable() && 'cursor-default'} ${buyable() && 'opacity-100 hover:bg-blue-400 '} ${anim() && 'animate-button'}`} onClick={() => {
+                        buyMe();
+                        setAnim(true);
+                    }} onAnimationEnd={() => setAnim(false)}>
+
+                    <span class='font-extrabold'>{textOverflow(cost)} R</span>
                 </button>
             </div>
         </div>
