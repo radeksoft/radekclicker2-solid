19c96fb5084fd5f9d8cba52ccacb93ee739fd256
chamik <jakub.hampl@email.cz>
2022-12-20 13:19:01 +0100

    Fixed text overflow in counter

--- src/components/radek-count.tsx
+++ src/components/radek-count.tsx
@@ -38,8 +38,8 @@ const textOverflow = (n: number) => {
 
 export const RadekCount: Component = () => {
     return (
-        <div class="flex flex-col items-center w-96 sm:w-[500px] bg-black/[.15] rounded-lg overflow-clip">
-            <p class="font-bold text-5xl sm:text-7xl pt-4">{textOverflow(radekCount()).replace('.', ',')}</p>
+        <div class="flex flex-col items-center w-80 sm:w-[500px] bg-black/[.15] rounded-lg overflow-clip">
+            <p class="font-bold text-5xl sm:text-7xl pt-4 text-clip text-center">{textOverflow(radekCount()).replace('.', ',')}</p>
             <p class='mt-1 text-xs font-bold'>{wordForm(Math.floor(radekCount()), ['Radekův Yen', 'Radekovy Yeny', 'Radekových Yenů'])}</p>
             <p class='my-1 pb-3 font-bold'>{textOverflow(radeksPerSecond()).replace('.', ',')} R/s</p>
         </div>
