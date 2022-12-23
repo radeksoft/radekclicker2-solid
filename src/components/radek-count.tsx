c25484fcc4bd8858f38b38db929ec5f5c691f47f
chamik <jakub.hampl@email.cz>
2022-12-23 18:44:06 +0100

    Added shadow to counter

--- src/components/radek-count.tsx
+++ src/components/radek-count.tsx
@@ -5,7 +5,7 @@ import { radekCount, radeksPerSecond, textOverflow, wordForm } from '../game';
 
 export const RadekCount: Component = () => {
     return (
-        <div class="flex flex-col items-center w-[22rem] sm:w-[500px] bg-black/[.15] rounded-lg overflow-clip">
+        <div class="flex flex-col items-center w-[22rem] sm:w-[500px] bg-black/[.15] rounded-lg overflow-clip shadow-md">
             <p class="font-bold text-5xl sm:text-7xl pt-4 text-clip text-center">{textOverflow(radekCount()).replace('.', ',')}</p>
             <p class='mt-1 text-xs font-bold'>{wordForm(Math.floor(radekCount()), ['Radekův Yen', 'Radekovy Yeny', 'Radekových Yenů'])}</p>
             <p class='my-1 pb-3 font-bold'>{textOverflow(radeksPerSecond()).replace('.', ',')} R/s</p>
